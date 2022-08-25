0814c53e4ded825e9295ada931ead4ce6f08b202
sarahelmoukadem <sara.mokadm@iqraaly.com>
2022-08-25 13:07:37 -0700

    create header component

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,25 @@
+import { HomeIcon, InformationCircleIcon, PhoneIcon, UserIcon } from '@heroicons/react/24/solid';
+import Image from "next/image";
+import HeaderIcon from './HeaderIcon';
+
 const Header = () => {
-    return ( 
-        <h1>Header</h1>
-     );
+    return (
+        <>
+            <div className="">
+                <HeaderIcon Icon={HomeIcon} title="Home" />
+                <HeaderIcon Icon={UserIcon} title="Account" />
+                <HeaderIcon Icon={PhoneIcon} title="Contact" />
+                <HeaderIcon Icon={InformationCircleIcon} title="About" />
+            </div>
+            <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/640px-IMDB_Logo_2016.svg.png"
+                width={100}
+                height={100}
+
+
+            />
+        </>
+    );
 }
- 
+
 export default Header;
---
+++ components/HeaderIcon.js
@@ -0,0 +1,13 @@
+const HeaderIcon = ({ Icon, title }) => {
+    return (
+        <>
+        <Icon className="h-8"/>
+        <p>
+            {title}
+        </p>
+
+        </>
+    );
+}
+
+export default HeaderIcon;
