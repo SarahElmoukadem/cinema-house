b6fa788b24a3cb759b05dd5dda4e7df071a5f9a4
sarahelmoukadem <sara.mokadm@iqraaly.com>
2022-08-26 00:34:03 -0700

    enhance UI like card component and responsive

--- components/Card.js
+++ components/Card.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Card = ({ result }) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original";
 
-    return (<div className="p-3">
+    return (<div className="p-3 cursor-pointer hover:text-white active:text-red-400 xl:hover:scale-105 transition-transform duration-200">
         <Image
             src={BASE_URL + result.backdrop_path}
             layout='responsive'
@@ -12,18 +12,18 @@ const Card = ({ result }) => {
             height={100}
         />
 
-        <div className="">
-            <p className="truncate">
+        <div className="p-2">
+            <p className="truncate text-lg">
                 {result.overview}
             </p>
-            <h2>
+            <h2 className="font-bold text-lg">
                 {result.title || result.name}
             </h2>
-            <p>
+            <p className="flex items-center">
                 {result.release_date || result.first_air_date}
+                <HandThumbUpIcon className="h-5 ml-3 mr-1" />
+                {result.vote_count}
             </p>
-            <HandThumbUpIcon className="h-5"/>
-            {result.vote_count}
         </div>
     </div>);
 
