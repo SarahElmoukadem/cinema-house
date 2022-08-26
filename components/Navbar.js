a62bb72c97d1f861a8ece964cf5274a2d44f2bd2
sarahelmoukadem <sara.mokadm@iqraaly.com>
2022-08-25 18:39:48 -0700

    create and handle navbar and requests.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,19 @@
+import { useRouter } from "next/router";
+import requests from '../utils/requests';
+
 const Navbar = () => {
+    const router = useRouter()
     return ( 
-        <h2>Navbar</h2>
+        <div className='flex justify-center bg-gray-600 text-gray-200 select-none text-xl lg:text-2xl'>
+            {Object.entries(requests).map(([key, {title,url}]) => 
+            <h2 
+            onClick={() => (router.push(`?genre=${key}`))}
+            className="m-6 cursor-pointer hover:text-white active:text-red-400"
+             key={key}>
+                {title}
+            </h2>
+            )}
+        </div>
      );
 }
  
